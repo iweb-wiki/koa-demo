5a2b629bc3286bbf493970f772cba51b58a7effc
PanSheng <21289700+iweb-wiki@users.noreply.github.com>
2020-07-04 20:10:02 +0800

    init

--- dist/server.js
+++ dist/server.js
@@ -1,13 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Koa = require("koa");
-const Router = require("koa-router");
+// import * as Router from "koa-router";
 const app = new Koa();
-const router = new Router();
-router.get('/*', async (ctx) => {
-    ctx.body = 'Hello World!';
-});
-app.use(router.routes());
-app.listen(8000);
+// const router = new Router();
+// router.get('/*', async (ctx: any) => {
+//     ctx.body = 'Hello World!';
+// })
+// app.use(router.routes());
+app.use(ctx => [
+    ctx.body = '33434'
+]);
+module.exports = app.listen(8000);
 console.log('Server running on port 8000');
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
 import * as Koa from "koa";
-import * as Router from "koa-router";
+// import * as Router from "koa-router";
 
 const app = new Koa();
-const router = new Router();
+// const router = new Router();
 
-router.get('/*', async (ctx: any) => {
-    ctx.body = 'Hello World!';
-})
+// router.get('/*', async (ctx: any) => {
+//     ctx.body = 'Hello World!';
+// })
 
-app.use(router.routes());
+// app.use(router.routes());
 
-app.listen(8000);
+app.use(ctx => [
+    ctx.body = '33434'
+])
+
+module.exports = app.listen(8000);
 
 console.log('Server running on port 8000')
